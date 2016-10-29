c1567891a0d2d4dbb6b0dbfd7931f60ebb854088
andreea0496 <andreea.evdochim@yahoo.com>
2016-10-29 21:58:59 +0300

    Update E4_proiect1.m

--- E4_proiect1.m
+++ E4_proiect1.m
@@ -22,30 +22,28 @@ x1 = sin (n1*pi / 17);      %am initializat vectorul x1 care depinde de functia 
 n2 = 0:50;
 x2 = cos (n2*pi / sqrt(23));    
 
-figure(1)
+figure(1)   %in figure(1) am reprezentat cele doua grafice suprapuse
 
 plot(x1,'r') 
-hold on
+hold on   %am folosit hold on pentru a retine graficul facut pentru a putea suprapune cel de al doilea grafic
 plot(x2,'k'), grid, title('Sinus si Cosinus')
-hold off
+hold off  %am folosit hold off pentru a continua reprezentarea grafica intr-o alta fereastra
 
-figure(2)
+figure(2)  %am reprezentat aici cele doua grafice in doua miniferestre fara a se suprapune
 
 subplot(2,1,1), plot(x1,'r'), grid, title('Sinus')
 subplot(2,1,2), plot(x2,'k'), grid, title('Cosinus')
+
+%in figurile 3 si 4 am reprezentat graficelede mai sus in varianta discreta, folosind functia 'stem' 
 
 figure(3)
 
 stem(x1,'k')
 hold on 
-stem(x2,'m')
+stem(x2,'m'), grid
 hold off
 
 figure(4)
 
 subplot(2,1,1), stem(x1,'k'), grid, title('Sinus discret')
 subplot(2,1,2), stem(x2,'m'), grid, title('Cosinus discret')
-
-
-
-
