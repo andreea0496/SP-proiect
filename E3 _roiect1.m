75c61006c2e2b631bdce3ad2c010f7c76f8bb07e
andreea0496 <andreea.evdochim@yahoo.com>
2016-10-29 19:43:21 +0300

    Add files via upload

---
+++ E3 _roiect1.m
@@ -0,0 +1,17 @@
+a=0:0.1:2
+b=linspace(11,11,21)
+b=zeros(21,1)
+b=b*11
+b=ones(21,1)
+b=b*11
+c=a*b
+[2 3 4]*[1;2;3]
+c=b*a
+a
+b
+b*a
+[1;2;3]*[2 3 4]
+x = 0:0.1:10;
+y = gaussmf(x,[2 5]);
+plot(x,y)
+xlabel('gaussmf, P=[2 5]')
